c112ee0b77ac524a903e9127b56b3adbb23c84e0
PG <goclowskipiotr@gmail.com>
2022-09-23 14:31:37 +0200

    hacker

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return "Hello {}! from Cloud Days !!!".format(name)
+    return '<title>helloRun</title><center><img src=/static/helloworld.jpeg width="640" alt="hackerman!"></center>'
+    
+    #return "Hello {}!".format(name)
 
 
 if __name__ == "__main__":
