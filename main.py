5fe02bf334bd53397cb2e09dc1d275f2b4f64ca4
PG <goclowskipiotr@gmail.com>
2022-09-23 14:35:15 +0200

    hello

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return '<title>helloRun</title><center><img src=/static/helloworld.jpeg width="640" alt="hackerman!"></center>'
-    
-    #return "Hello {}!".format(name)
+    #return '<title>helloRun</title><center><img src=/static/helloworld.jpeg width="640" alt="hackerman!"></center>'
+    return "Hello {}!".format(name)
 
 
 if __name__ == "__main__":
